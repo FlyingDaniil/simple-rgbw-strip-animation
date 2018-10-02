4451866851d1ed9ca69426fc398ad93c22ab7a2c
zhucharych <zhuk914@gmail.com>
2018-10-02 23:04:03 +0200

    v1

---
+++ s2/s2.ino
@@ -0,0 +1,22 @@
+#define redPin 2
+#define greenPin 3
+#define bluePin 4
+#define whitePin 5
+
+#define speedRegulator A0
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(grrenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(whitePin, OUTPUT);
+  pinMode(speedRegulator, INPUT);
+}
+
+void loop() {
+  analogWrite(redPin, random(1023));
+  analogWrite(grrenPin, random(1023));
+  analogWrite(bluePin, random(1023));
+  analogWrite(whitePin, random(1023));
+  delay(analogRead(speedRegulator));
+}
